0923f056351869a631efce35eff7aa2e55626daa
Muhammad Albert Nur Agathon <muhammadalbert16@gmail.com>
2023-11-01 19:38:48 +0700

    fix warning

--- lib/screen/splash/splash_view_model.dart
+++ lib/screen/splash/splash_view_model.dart
@@ -8,20 +8,28 @@ class SplashViewModel extends ChangeNotifier {
   Future<void> checkLogin(BuildContext context) async {
     final user = await SharedPreferencesUtils().getToken();
     if (user.isNotEmpty) {
-      Navigator.pushAndRemoveUntil(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const HomeScreen(),
-        ),
-        (route) => false,
+      Timer(
+        const Duration(seconds: 2),
+        () {
+          Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+            ),
+            (route) => false,
+          );
+        },
       );
     } else {
-      Navigator.pushAndRemoveUntil(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const LoginScreen(),
-        ),
-        (route) => false,
+      Timer(
+        const Duration(seconds: 2),
+        () {
+          Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+              builder: (context) => const LoginScreen(),
+            ),
+            (route) => false,
+          );
+        },
       );
     }
   }
